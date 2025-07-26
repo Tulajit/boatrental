7d1c78fe8ed844bfc2edd03e10bfd17016eb7beb
Tulajit <60899264+Tulajit@users.noreply.github.com>
2025-07-26 15:33:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def logout():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
